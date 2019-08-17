fbef1a4140404fe21bbdc4193006ca753516bf25
JD2 <jdelisle@setseg.org>
2019-08-17 11:39:29 -0400

    My First One Actually

---
+++ Model/Blog.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace LucidLogic.Model
+{
+    public class Blog
+    {
+        public int ID {get; set;}
+        public string Title {get; set;}
+
+        public string BlogPost{get; set;}
+
+        public DateTime PostDate{get; set;}
+    }
+}
